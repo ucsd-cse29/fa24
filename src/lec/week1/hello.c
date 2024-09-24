417ef802097e25aba4abf127096ec5a779cae70e
Joe Politz <joe.politz@gmail.com>
2024-09-24 15:46:06 -0700

    hello

---
+++ src/lec/week1/hello.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+  char hello[] = "Hello!"; 
+  char hello2[] = { 72, 101, 108, 108, 111, 33, 0 };
+
+  puts(hello);
+  puts(hello2);
+}
