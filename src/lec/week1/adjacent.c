b2b0719c73f34342d73aeafa773b5b20264b05ca
Joe Politz <joe.politz@gmail.com>
2024-09-24 22:30:08 -0700

    adjacent

---
+++ src/lec/week1/adjacent.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    char hello[] = { 72, 101, 108, 111, 111, 33, 33, 33 };
+    char hello2[] = { 72, 101, 108, 111, 111, 33, 33, 33 };
+    puts(hello);
+    puts(hello2);
+}
